f54c8ce6aa0d2a14a4cb1e81fc87b73b76070fdd
irsathameen <irsathameen@rediffmail.com>
2024-11-16 12:47:32 +0530

    Newly added files

---
+++ EmployeePortal/Controllers/LoginController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeePortal.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        [Route("validateUsers"),HttpGet]
+        public ActionResult<string> ValidateUsers()
+        {
+
+            return "Success";
+        }
+
+    }
+}
